202f930cd9160e39741b16d4609e9bc539429ec6
Daniel Ortiz <jdoavila@gmail.com>
2014-03-05 22:28:49 -0600

    urls cambios

--- webapp/urls.py
+++ webapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^API/getreports/$', views.getreports, name='getreports'),
 
     url(r'^http://dbriones.com/sitios/reportit/API/getreports/$', views.getreports, name='getreports'),
+    url(r'^sitios/reportit/API/getreports/$', views.getreports, name='getreports'),
     
     url(r'^API/getcategories/$', views.getcategories, name='getcategories')
     url(r'^API/getcountries/$', views.getcountries, name='getcountries')
